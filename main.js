3a75ffda1f09f2121ac2954216e65bef5fbac811
aman-pandia <amanpandia05@gmail.com>
2020-04-14 16:41:35 +0530

    New UI

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1080,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
